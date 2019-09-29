bef85e37cca04699ff80da0055d045facd75cb09
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-09-28 23:44:56 -0500

    Spring Constructor Injection

--- Spring Dependency Injection/src/com/spring/DI/Student.java
+++ Spring Dependency Injection/src/com/spring/DI/Student.java
@@ -3,17 +3,29 @@ package com.spring.DI;
 public class Student {
 	
 	private String StudentName;
+	private int id;
+	private String Email;
+	private int mobileno;
 	
-	
-	
+
+	public Student(int id, String Email) {
+		
+		this.id = id;
+		this.Email = Email;
+	}
+
 	public void setStudentName(String studentName) {
-		StudentName = studentName;
+		this.StudentName = studentName;
+	}
+
+	public void setMobileno(int mobileno) {
+		this.mobileno = mobileno;
 	}
 
 
-
 	public void displayStudentInfo() {
-		System.out.println("StudentName is: " + StudentName);
+		System.out.println("StudentName is: " + StudentName + " and id is: " + id 
+				+ " email is: " + Email + " mobileno is: " + mobileno);
 	}
 	
 
